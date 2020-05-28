bc273b261b57ecb8728f7bc796f84a89ff67fcde
rockfirmman <1424602142@qq.com>
2020-05-28 13:37:40 +0800

    移动脚步声

--- Assets/Script/Audio/Volume_Control.cs
+++ Assets/Script/Audio/Volume_Control.cs
@@ -6,7 +6,7 @@ public class Volume_Control : MonoBehaviour
 {
     public AudioSource audioSource;
     // Update is called once per frame
-    void Update()
+    void Start()
     {
         audioSource.volume = Global.VOLUME;
     }
